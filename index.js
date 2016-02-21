1a21d82c8745916924de581cce23011af2f69bdd
davidkatz <15Dkatz@shcp.edu>
2016-02-20 23:55:26 -0800

    First Commit

--- index.js
+++ index.js
@@ -1,106 +1,99 @@
-var mouseX =0, mouseY = 0;
-var randSize;
+//commit!!
 
-var colorCollection = ["#e91e63", "#2196f3", "#fff", "#fff", "#fff", "#03a9f4", "#ffeb3b", "#ef5350"]
+var colorCollection = ["#fff", "#fff", "#fff", "#fff", "#e91e63", "#2196f3", "#fff", "#fff", "#fff", "#03a9f4", "#ffeb3b", "#ef5350"]
 
 //generate multiple children.
+var generateBubble = function(x, y, color, size) {
+	console.log("blowing a bubble!");
+	var bubble = document.createElement('div');
+
+	// var randColor = colorCollection[Math.floor(Math.random()*colorCollection.length)];
+	bubble.style.background = color;
+	bubble.style.left = x;
+	bubble.style.top = y;
+	bubble.className = "bubble";
+	
+
+	if (size===undefined) {
+		size=10;
+	}
+	bubble.style.width=size;
+	bubble.style.height=size;
+	bubble.style.left = x-size/2;
+	bubble.style.top = y-size/2+1;
+
+	$("#bubbleParent").append(bubble);
+	setTimeout(function(){bubble.style.display = "none"}, 950);
+
+}
+
 
 $(document).ready(function() {
+	var randColor, stableColor;
+	var mouseX=0, mouseY=0;
+	var stableX=0, stableY=0;
+	var blow;
+	var colorLim=0;
 
-	var move;
-	var clone;
-	// var travel = function(x, y) {
-
-	// }
-
-
-	// $( ".hello" ).clone().appendTo( ".goodbye" );
-
-
-	$(document).mousedown(function(e) {
-		mouseX = e.pageX;
-		mouseY = e.pageY;
-
-		console.log(mouseX, mouseY);
-
-		randSize = Math.floor(Math.random()*65)+10;
-		randColor = Math.floor(Math.random()*colorCollection.length);
-
-		var globalX = mouseX-(randSize/2), globalY = mouseY-(randSize/2);
-
-		$(".bubbleChild").removeClass("hide");
-		$(".bubbleChild").addClass("bubble");
-		$(".bubbleChild").css({
-			left: globalX,
-			top: globalY,
-			width: randSize,
-			height: randSize,
-			background: colorCollection[randColor],
-		});
-
-
-		var globalSpeed = 1;
-		var changeSpeed = function() {
-			// console.log("changing speed");
-			globalSpeed = Math.floor(Math.random()*3)-1.5;
-		}
-
-		var globalLimit=0;
-
-		//travel
-		var travel = function() {
-			// console.log("changing position");
-			globalX+=globalSpeed;
-
-			if(globalSpeed>0) {
-				globalY+=1-globalSpeed;
-			} else {
-				globalY+=globalSpeed+1;
-			}
-			// console.log(globalX, "x", globalY, "y");
-			$(".bubbleChild").css({
-				left: globalX,
-				top: globalY,
-			})
-			globalLimit+=10;
-			console.log(globalLimit, "limit");
-			if (globalLimit>800) {
-				console.log("remove!");
-				$(".bubbleChild").removeClass("bubble");
-				$(".bubbleChild").addClass("hide");	
-			}
-		}
-
-		// var clone = function() {
-		// 	console.log("cloning!");
-		// 	// $(".bubbleChild").clone().appendTo("#bubbleParent");
-		// 	$(".bubbleChild").addClass("show");	
-		// }
-
-
-		move = setInterval(travel, 10);
-		changeSpeed();
-		// clone = setInterval(clone, 1000);
-
+	$(document)
+	.mousedown(function(f) {
+		limit=0;
+		var bubbleSize;
+		stableColor = "#fff";
+		mouseX=f.pageX;
+		mouseY=f.pageY;
+		//change X and Y in random directions
 		
+		var blowBub = function() {
+			limit+=1;
+			if (limit>100) {
+				// console.log("colorLim breached");
+			}
+			
+			//new spot every 10 ms
+			if (limit%10===0) {
+				var randX=Math.floor(Math.random()*20)-10;
+				var randY=Math.floor(Math.random()*20)-10;
 
+				mouseX = f.pageX+randX;
+				mouseY = f.pageY+randY;	
+			}
+			if (limit%25===0&&limit>30) {
+				stableColor = colorCollection[Math.floor(Math.random()*colorCollection.length)];
+
+			}
+			bubbleSize= (30/(limit/100)+1);
+
+			if (bubbleSize>30) {
+				bubbleSize=30;
+			} else if (bubbleSize<10) {
+				bubbleSize=10;
+			}
+
+
+
+			generateBubble(mouseX, mouseY, stableColor, bubbleSize);
+		}
+		blow = setInterval(blowBub, 10);
 
 
 	})
-	.mousemove(function(f) {
-	    mouseX = f.pageX;
-	    mouseY = f.pageY;
-	    $(".bubbleChild").css({
-		    left: mouseX-(randSize/2),
-			top: mouseY-(randSize/2)
-	    })
+	.mousemove(function(e) {
+		randColor = colorCollection[Math.floor(Math.random()*colorCollection.length)];
+		mouseX=e.pageX;
+		mouseY=e.pageY;
+
+		if ((Math.abs(mouseX-stableX)>100)&&(Math.abs(mouseY-stableY)>100)) {
+			generateBubble(mouseX, mouseY, randColor);
+		}
 	})
 	.mouseup(function() {
-		clearInterval(move);
-		// clearInterval(clone);
-		$(".bubbleChild").removeClass("bubble");
-		$(".bubbleChild").addClass("hide");
-	}) 
-
-
+		clearInterval(blow);
+	})
 });
+
+
+
+
+//create divs, append to bubbleParent,
+//id = bubbleChild[1]
